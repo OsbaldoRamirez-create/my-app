70edb657311c35c462b40f09221439b683fdee78
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2024-12-23 18:34:01 -0800

    created home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
-function App() {
+import './App.css';
+import Home from './Home';
+
+
+export default function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     
+      <Routes> 
+
+        <Route path='/' element={<Home/>}></Route>
+       
+      </Routes>
+    
+    
     </div>
+    </Router>
   );
 }
 
-export default App;
