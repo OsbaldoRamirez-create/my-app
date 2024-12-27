105209f2a0ea95b854c47ae3581bb7bf492b032d
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2024-12-27 14:48:42 -0800

    added navabar to all pages

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import './App.css';
 import Home from './Home';
+import Navbar from './Components/Navbar';
 
 
 export default function App() {
   return (
     <Router>
     <div className="App">
-      
+      <Navbar/>
      
       <Routes> 
 
