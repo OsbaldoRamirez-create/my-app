279920e86eda384b6006ef6414ff876991704431
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-01-02 17:54:12 -0800

    created footer

---
+++ src/Components/FooterBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Styles/FooterBarStyles.css';
+import { NavLink } from 'react-router-dom';
+
+export default function FooterBar() {
+  return (
+    <footer>
+        <NavLink to="/">Home</NavLink>                
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/services">Services</NavLink>
+        <NavLink to="/contact">Contact us</NavLink>
+    </footer>
+  )
+}
+
