0fa303e68613540068eec235beafe5e527c8254b
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-01-20 13:06:20 -0800

    added about to app

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import './App.css';
 import Home from './Home';
+import About from './About';
 import Navbar from './Components/Navbar';
 import FooterBar from './Components/FooterBar';
 
@@ -15,6 +16,7 @@ export default function App() {
       <Routes> 
 
         <Route path='/' element={<Home/>}></Route>
+        <Route path='/About' element={<About/>}></Route>
        
       </Routes>
       
