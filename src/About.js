22c73a05e7a5f787bf624173f1c1df43cbe518b4
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-01-20 13:06:09 -0800

    started about us page

---
+++ src/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="about-container">
+            <h3> Why choose us?</h3>
+            <p>
+                At Ramirez Landscaping, we bring your vision to life while giving fair prices. We have been maintaining and remaking yards for over 15 years all over the bay area
+                even having loyal customers since we started. 
+                We are a family owned and operated business that offers various services such as yard maintenance, fencing, irrigation systems, tree-work and more!
+                Each yard is unique so we take the time to hear what our clients are looking for so we can satisfy them while staying in their budget. 
+                Fill out the info below so that our family can start working with you and transforming your yard! 
+            </p> 
+        </div>
+  )
+}
