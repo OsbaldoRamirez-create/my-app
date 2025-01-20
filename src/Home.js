69326620fd8891fbb828700bf9b29db5fdebd99e
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-01-20 13:16:08 -0800

    added quote component to home

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import grassImg from './Images/NewGrass2.png';
 import patioImg from './Images/cleanerPatio.png';
 import './Styles/HomeStyles.css';
 import Slideshow from './Components/Slideshow';
+import Quote from './Components/Quote';
 import { NavLink } from 'react-router-dom';
 
 export default function Home() {
@@ -50,36 +51,8 @@ export default function Home() {
         </div>
         
     </div>
+    <Quote/>
 
-    <div className="quote-container">
-        <h1> Request a Consultation</h1>
-        <form id="landscaping-form">
-            <div className="input-container">
-                <div className="form-class">
-                    <input type="text" id="name" name="name" placeholder="Name*" required/>
-                </div>
-                <div className="form-class">
-                    <input type="tel" id="phone" name="phone" placeholder="Phone Number*" required/>
-                </div>
-                <div className="form-class">
-                    <input type="email" id="email" name="email" placeholder="Email*" required/>
-                </div>
-                <div className="form-class">
-                    <input type="text" id="subject" name="subject" placeholder="Subject*" required/>
-                    <h4> *Free quotes only in the Bay Area CA all other locations subject to a fee depending on location</h4> 
-                </div>
-                </div>
-            <div className="description-container">
-                <div className="form-class">
-                    <textarea id="description" name="description" placeholder="Description:"></textarea>
-                </div>
-            </div>
-        <div className="btn-container">
-        <button type="submit"> Submit</button>
-        </div>
-    </form>
-    </div>
-    
      </div>
   )
 }
