a5768aae6936a55b781b0770fb7831637d689bf6
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-01-31 10:50:17 -0800

    added maintenance info section

--- src/MaintenancePage.js
+++ src/MaintenancePage.js
@@ -8,6 +8,18 @@ export default function MaintenancePage() {
         <img src={RamirezLogo} className="ramirezLogo"alt="Logo"/>
     
         <h1>Yard Maintenance Services</h1>
+        <div className='maintenance-info'>
+            <p>
+                At Ramirez Landscaping, we offer a variety of services to help you maintain your yard whether 
+                it's weekly, bi-weekly, or monthly. Our team is committed to enhancing the beauty of your 
+                outdoor spaces, ensuring that each visit leaves your property looking pristine and well-kept.
+                We treat every yard as it if were our own, taking pride in our attention to detail. 
+                Utilizing the best equipment and latest techniques, we care for your yard efficiently and effectively. 
+                With Ramirez Landscaping, you receive not just a service provider, but a dedicated partner
+                who values your satisfaction and the health of your landscape. Hire us today and experience the difference we can make!
+                Let our family maintain your yard, so you can enjoy your beautiful garden without the hassle.
+            </p>
+        </div>
         <div className='maintenance-services'>
             <h2> Our services include:</h2>
             <div className='yard-services'>
