5b6cd04484d0b11b17e89965e8c83ccca4615068
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-03-12 16:17:08 -0700

    made slideshow reusable for diff images

--- src/Components/Slideshow.js
+++ src/Components/Slideshow.js
@@ -1,21 +1,7 @@
 import React, {useState, useEffect}from 'react'
 import '../Styles/SlideshowStyles.css';
-import image1 from '../Images/SlabsLandscape.png';
-import image2 from '../Images/RetainingWallAndGrass.png';
-import image3 from '../Images/BackLawn.png';
-import image4 from '../Images/BarkAndRockSlabs.png';
-import image5 from '../Images/fence.png';
 
-
-const yardImages = [
-{src: image1, alt:"Front yard slabs", text:"Rocks Slabs Landscape"},
-{src: image2, alt:"Retaining wall", text:"Retaining Wall and Artifical Lawn"},
-{src: image3, alt:"Backyard Lawn", text:"Back yard lawn"},
-{src: image4, alt:"Bark and slabs", text:"Bark and Rock Slabs"},
-{src: image5, alt:"Backyard Fence", text:"New Redwood fence"}
-]
-
-const Slideshow = ({images = yardImages}) => {
+const Slideshow = ({images}) => {
     
     const [slideIndex, setSlideIndex]=useState(0);
 useEffect(() => {
