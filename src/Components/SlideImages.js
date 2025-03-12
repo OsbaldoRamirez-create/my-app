fc511c5f1695da492cf13039e2e4f6bbf6de7efb
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-03-12 16:17:27 -0700

    slideshow images

---
+++ src/Components/SlideImages.js
@@ -0,0 +1,23 @@
+import image1 from '../Images/SlabsLandscape.png';
+import image2 from '../Images/RetainingWallAndGrass.png';
+import image3 from '../Images/BackLawn.png';
+import image4 from '../Images/BarkAndRockSlabs.png';
+import image5 from '../Images/fence.png';
+import image6 from "../Images/Grass.jpg";
+
+export const yardImages = [{src: image1, alt:"Front yard slabs", text:"Rocks Slabs Landscape"},
+{src: image2, alt:"Retaining wall", text:"Retaining Wall and Artifical Lawn"},
+{src: image3, alt:"Backyard Lawn", text:"Back yard lawn"},
+{src: image4, alt:"Bark and slabs", text:"Bark and Rock Slabs"},
+{src: image5, alt:"Backyard Fence", text:"New Redwood fence"}
+]
+
+
+export const yardImages2 = [{src: image1, alt:"Front yard slabs", text:"Rocks Slabs Landscape"},
+    {src: image6, alt:"Front Yard Sod", text:"New Front Yard Sod"},
+    {src: image3, alt:"Backyard Lawn", text:"Back yard lawn"},
+    {src: image4, alt:"Bark and slabs", text:"Bark and Rock Slabs"},
+    {src: image5, alt:"Backyard Fence", text:"New Redwood fence"}
+    ]
+    
+    
