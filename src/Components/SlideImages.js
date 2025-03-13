c755b9a90b0a8409da3cefd4d657605ab6f92b77
OsbaldoRamirez-create <o_ramirez3@u.pacific.edu>
2025-03-12 18:12:24 -0700

    updated images for slideshows

--- src/Components/SlideImages.js
+++ src/Components/SlideImages.js
@@ -4,20 +4,25 @@ import image3 from '../Images/BackLawn.png';
 import image4 from '../Images/BarkAndRockSlabs.png';
 import image5 from '../Images/fence.png';
 import image6 from "../Images/Grass.jpg";
+import image7 from "../Images/LongFence.jpg";
+import image8 from "../Images/Mulch.jpg";
+import image9 from "../Images/Patio.jpg";
+import image10 from "../Images/CleanTree.jpg";
 
-export const yardImages = [{src: image1, alt:"Front yard slabs", text:"Rocks Slabs Landscape"},
+export const yardImages = [{src: image1, alt:"Front Yard Slabs", text:"Rocks Slabs Landscape"},
 {src: image2, alt:"Retaining wall", text:"Retaining Wall and Artifical Lawn"},
-{src: image3, alt:"Backyard Lawn", text:"Back yard lawn"},
+{src: image3, alt:"Backyard Lawn", text:"New Back Yard Sod"},
 {src: image4, alt:"Bark and slabs", text:"Bark and Rock Slabs"},
 {src: image5, alt:"Backyard Fence", text:"New Redwood fence"}
 ]
 
 
-export const yardImages2 = [{src: image1, alt:"Front yard slabs", text:"Rocks Slabs Landscape"},
-    {src: image6, alt:"Front Yard Sod", text:"New Front Yard Sod"},
-    {src: image3, alt:"Backyard Lawn", text:"Back yard lawn"},
-    {src: image4, alt:"Bark and slabs", text:"Bark and Rock Slabs"},
-    {src: image5, alt:"Backyard Fence", text:"New Redwood fence"}
+export const yardImages2 = [
+    {src: image6, alt:"Front yard sod", text:"New Front Yard Sod"},
+    {src: image7, alt:"Long Fence", text:"70 Foot Fence"},
+    {src: image8, alt:"Front Yard mulch", text:"Front Yard Mulch and clean-up"},
+    {src: image9, alt:"Brick Patio", text:"Brick Patio"},
+    {src: image10, alt:"Clean Tree", text:"Perfectly Trimmed Tree"}
     ]
     
     
